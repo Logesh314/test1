d1deaa90308e87abde86e494c173cc61525023ac
logesh314 <logeshkumar1918@gmail.com>
2024-04-16 23:19:20 +0530

    subraction function is added

--- sum.py
+++ sum.py
@@ -1,4 +1,11 @@
+def sum(x,y):
+	return a + b
+
+def sub(x,y):
+	return a - b
+
 a = int(input("Enter number 1:"))
 b = int(input("Enter number 2:"))
 
-print("sum:",a+b)
+print("sum:",sum(a,b))
+print("subtraction:",sub(a,b))
